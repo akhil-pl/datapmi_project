e78cb54de518d8eebd68c91cd912b666a6b03046
Akhil P L <akhilplx@gmail.com>
2023-11-13 15:24:58 +0530

    file path from .env

--- functions/dbt_yml_file_func.py
+++ functions/dbt_yml_file_func.py
@@ -1,12 +1,23 @@
 # Need to add functions for manipulating .yml files here
 
+import os
 import yaml
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 
 # Function to read profiles.yml file
 def read_profiels_yml():
     try:
-        with open('/home/user/.dbt/profiles.yml', 'r') as file: # Read the YAML file
+        # Get the file path from the environment variable
+        file_path = os.getenv('DBT_PROFILES_PATH')
+
+        if not file_path:
+            raise ValueError("DBT_PROFILES_PATH is not set in the .env file")
+
+        with open(file_path, 'r') as file: # Read the YAML file
             data = yaml.load(file, Loader=yaml.FullLoader)
     except Exception as e:
         # Handle exceptions, (log the error or raise a custom exception)
@@ -19,7 +30,13 @@ def read_profiels_yml():
 # Function to write to profiles.yml file
 def write_profiles_yml(data):
     try:
-        with open('/home/user/.dbt/profiles.yml', 'w') as file:
+        # Get the file path from the environment variable
+        file_path = os.getenv('DBT_PROFILES_PATH')
+
+        if not file_path:
+            raise ValueError("DBT_PROFILES_PATH is not set in the .env file")
+
+        with open(file_path, 'w') as file:
             yaml.dump(data, file, default_flow_style=False)
     except Exception as e:
         # Handle exceptions, (log the error or raise a custom exception)
